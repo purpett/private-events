dc9e459ce7edafa37a7306a5b7d8d5837023b6fb
michela <michelab299@hotmail.it>
2022-08-31 14:58:17 +0100

    create controller for show, new, create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,27 @@ class EventsController < ApplicationController
     @events = Event.order(event_date: :desc)
   end
 
+  def show
+    @events
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to events_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def event_params
+    params.require(:event).permit(:title, :description, :location, :event_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   root "events#index"
+  resources :events
 end
