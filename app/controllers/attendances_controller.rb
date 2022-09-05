517d6a37508f4893ef058cc979d8bfe17197f602
michela <michelab299@hotmail.it>
2022-09-05 14:55:36 +0100

    implement attend button to events

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -3,7 +3,7 @@ class AttendancesController < ApplicationController
     @event = Event.find(params[:event_id])
     @attendance = @event.attendances.build({attendee: current_user})    # attendances -> fill a line in the table attendances with a new attendance
 
-    @attendance.save                # not checking if save works cause no validation needed
+    @attendance.save!                # not checking if save works cause no validation needed
     redirect_to event_path(@event)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, foreign_key: :creator_id
-  has_many :events_users, foreign_key: :attendee_id
-  has_many :attended_events, through: :events_users
+  has_many :attendances, foreign_key: :attendee_id
+  has_many :attended_events, through: :attendances
        
   validates :username, presence: true, uniqueness: true, length: { in: 6..15, too_short: "too short. Minimum %{count} characters", too_long: "too long. Maximum %{count} characters allowed" }
 end
