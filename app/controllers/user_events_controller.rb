f92abdf3e0de1b2305b3129dc40a64cba0a73def
michela <michelab299@hotmail.it>
2022-09-07 11:40:28 +0100

    move queries for event division to view instead of controller

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -2,7 +2,6 @@ class UserEventsController < ApplicationController
   before_action :authenticate_user! 
 
   def index
-    @events = current_user.events.where("event_date >= ?", Date.today).order(event_date: :asc)
-    @passed_events = current_user.events.where("event_date < ?", Date.today).order(event_date: :asc)
+    @events = current_user.events
   end
 end
