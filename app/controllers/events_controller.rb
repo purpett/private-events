d5e96ad5f2274b73fcc97e199636e1153c866fbe
michela <michelab299@hotmail.it>
2022-09-07 14:05:58 +0100

    implement edit and update actions in Event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,9 +24,16 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
