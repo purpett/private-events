2964372b148582f1b942443f853f88027503d654
michela <michelab299@hotmail.it>
2022-09-07 14:49:08 +0100

    implement destroy action for attendances controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -6,4 +6,11 @@ class AttendancesController < ApplicationController
     @attendance.save!                # not checking if save works cause no validation needed
     redirect_to event_path(@event)
   end
+
+  def destroy 
+    @event = Event.find(params[:event_id])
+    @attendance = @event.attendances.find(params[:id])
+    @attendance.destroy
+    redirect_to event_path(@attendance.attended_event) # navigates back to event - whole point of associations (belongs_to)
+  end
 end
